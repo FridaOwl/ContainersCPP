0cd8f732bbb2104d3abd38bb3f790dabb8f9022b
FridaOwl <farvin.lori@gmail.com>
2024-04-24 01:08:29 +0300

    containers

---
+++ src/d3_containers.h
@@ -0,0 +1,14 @@
+#ifndef D3_CONTAINERS_SRC_
+#define D3_CONTAINERS_SRC_
+
+#include "BTree/tree.h"
+#include "array/d3_array.h"
+#include "list/d3_list.h"
+#include "map/d3_map.h"
+#include "multiset/d3_multiset.h"
+#include "queue/d3_queue.h"
+#include "set/d3_set.h"
+#include "stack/d3_stack.h"
+#include "vector/d3_vector.h"
+
+#endif  // D3_CONTAINERS_SRC_
